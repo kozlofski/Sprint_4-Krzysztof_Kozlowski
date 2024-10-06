476f863bf78013afdea4b0dadffebdf182dc072e
Krzysztof Kozlowski <kozlofski.dev@gmail.com>
2024-10-06 17:00:46 +0200

    Adding-new-task form working

--- todo-list.js
+++ todo-list.js
@@ -1,1 +1,14 @@
-console.log("Script loaded")
+const addTaskButton = document.querySelector(".add-task-button");
+const taskForm = document.forms['adding-new-task'];
+
+const newTaskInput = taskForm.elements['new-task-name'];
+
+console.dir(newTaskInput)
+
+const addTask = function(event) {
+    event.preventDefault();
+    const taskName = newTaskInput.value;
+    console.log(taskName)
+}
+
+taskForm.addEventListener('submit', addTask);
